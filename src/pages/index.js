a256a9a35047ddb797006a17d9f7e3f78637492e
hannahb09 <brown09@allegheny.edu>
2024-11-08 14:01:37 -0500

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import * as React from "react"
 import { Link } from "gatsby"
 export default function Home() {
 return <div style={{color:'tomato', backgroundColor: 'lightgray'}}>
-<h1>Hello world!</h1>
-<p>This is my first Gatsby Site</p>
-<img src="https://raw.githubusercontent.com/allegheny-
-college-cmpsc-104-Fall-2024/lab01/main/graphics/DocEng_logo.png"
-alt="Logo" width="150" />
-<br />
-<Link to="/page-2">Page 2</Link>
-<br />
-<Link to="/counter">counter</Link>
-</div>
-}
+    <h1>Hello world!</h1>
+    <p>This is my first Gatsby Site</p>
+    <img src="https://raw.githubusercontent.com/allegheny-
+    college-cmpsc-104-Fall-2024/lab01/main/graphics/DocEng_logo.png"
+    alt="Logo" width="150" />
+    <br />
+    <Link to="/page-2">Page 2</Link>
+    <br />
+    <Link to="/counter">counter</Link>
+    </div>
+    }
---
+++ src/pages/myfirstpost.md
@@ -0,0 +1,8 @@
+---
+title: "My Makrdownfile"
+authror: "Hannah"
+---
+
+This is the content in my markdown
+
+This is my cd test
